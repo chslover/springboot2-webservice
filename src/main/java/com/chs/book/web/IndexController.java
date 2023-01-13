e7ff7d15a984eaaf2e80ff3c5d2f4080209bc6d5
apple <chsloverr@naver.com>
2023-01-13 18:22:02 +0900

    chapter05.4 세션 로그인 어노테이션으로 개선

--- src/main/java/com/chs/book/web/IndexController.java
+++ src/main/java/com/chs/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.chs.book.web;
 
+import com.chs.book.config.auth.LoginUser;
 import com.chs.book.config.auth.dto.SessionUser;
 import com.chs.book.service.posts.PostsService;
 import com.chs.book.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
