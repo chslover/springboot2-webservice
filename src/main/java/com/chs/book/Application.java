27ffe741ddd67666742b3ccfd65ac0ad9f633498
apple <chsloverr@naver.com>
2023-01-13 23:05:16 +0900

    chapter05.7 테스트 코드에 시큐리티 적용

--- src/main/java/com/chs/book/Application.java
+++ src/main/java/com/chs/book/Application.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.swing.*;
-
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
